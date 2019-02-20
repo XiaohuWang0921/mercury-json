24f510f46cf1f543f8e0cee83faf0989369f6643
ToraNoOsama <tigger921@126.com>
2019-02-20 17:58:35 +0100

    Initial commit

---
+++ json.m
@@ -0,0 +1,282 @@
+% A simple JSON library for Mercury.
+
+:- module json.
+:- interface.
+
+:- import_module assoc_list, bool, /*char,*/ float, int, io, list, parsing_utils, string.
+
+:- type json.
+
+% Output format of a json object.
+
+:- type format
+	---> format(
+		object_spec::format_spec,
+		array_spec::format_spec
+	).
+
+% Format specifier.
+
+:- type format_spec
+	---> compact
+	;	 multiline(
+		indent::int,
+		newline::newline
+	).
+
+:- type newline
+	---> crlf
+	;	 cr
+	;	 lf.
+
+% Initialize a json.
+
+:- pred init_object(assoc_list(string, json)::in, json::out) is det.
+
+:- pred init_array(list(json)::in, json::out) is det.
+
+:- pred init_integer(int::in, json::out) is det.
+
+:- pred init_float(float::in, json::out) is det.
+
+:- pred init_string(string::in, json::out) is det.
+
+:- pred init_boolean(bool::in, json::out) is det.
+
+:- pred init_null(json::out) is det.
+
+:- func init_object(assoc_list(string, json)::in) = (json::out) is det.
+
+:- func init_array(list(json)::in) = (json::out) is det.
+
+:- func init_integer(int::in) = (json::out) is det.
+
+:- func init_float(float::in) = (json::out) is det.
+
+:- func init_string(string::in) = (json::out) is det.
+
+:- func init_boolean(bool::in) = (json::out) is det.
+
+:- func init_null = (json::out) is det.
+
+% Check the type of the content in a json.
+
+:- pred is_object(json::in) is semidet.
+
+:- pred is_list(json::in) is semidet.
+
+:- pred is_integer(json::in) is semidet.
+
+:- pred is_float(json::in) is semidet.
+
+:- pred is_string(json::in) is semidet.
+
+:- pred is_boolean(json::in) is semidet.
+
+:- pred is_null(json::in) is semidet.
+
+% Get the content of a json.
+
+:- func object(json::in) = (assoc_list(string, json)::out) is semidet.
+/*
+% Same warning as for object/1.
+:- func det_object(json) = assoc_list(string, json).
+*/
+:- func array(json::in) = (list(json)::out) is semidet.
+/*
+% Same warning as for list/1.
+:- func det_list(json) = list(json).
+*/
+:- func integer(json::in) = (int::out) is semidet.
+/*
+:- func det_integer(json) = int.
+*/
+:- func float(json::in) = (float::out) is semidet.
+/*
+:- func det_float(json) = float.
+*/
+:- func string(json::in) = (string::out) is semidet.
+/*
+:- func det_string(json) = string.
+*/
+:- func boolean(json::in) = (bool::out) is semidet.
+/*
+:- func det_boolean(json) = bool.
+*/
+% null has no content therefore no need for a predicate
+% to extract its content.
+
+% Conversion to string with a specified format.
+:- func to_string(json, format) = string.
+
+% Write a json to output with a specified format.
+:- pred write_json(json::in, format::in, io::di, io::uo) is det.
+:- pred write_json(io.text_output_stream::in, json::in, format::in, io::di, io::uo) is det.
+
+% Same but with the default format.
+/*
+:- func to_string(json) = string.
+
+:- pred write_json(json::in, io::di, io::uo) is det.
+:- pred write_json(io.text_output_stream::in, json::in, io::di, io::uo) is det.
+*/
+
+% A JSON parser.
+:- pred json_parser(src::in, json::out, ps::in, ps::out) is semidet.
+
+% Parse a JSON string.
+:- pred parse_json_string(string::in, parse_result(json)::out) is cc_multi.
+
+:- implementation.
+
+:- import_module pair.
+
+:- type json
+	--->	integer(int)
+	;		float(float)
+	;		string(string)
+	;		boolean(bool)
+	;		array(list(json))
+	;		object(assoc_list(string, json))
+	;		null.
+
+init_integer(Int, integer(Int)).
+
+init_float(Float, float(Float)).
+
+init_string(String, string(String)).
+
+init_boolean(Bool, boolean(Bool)).
+
+init_array(List, array(List)).
+
+init_object(AssocList, object(AssocList)).
+
+init_null(null).
+
+init_integer(Int) = integer(Int).
+
+init_float(Float) = float(Float).
+
+init_string(String) = string(String).
+
+init_boolean(Bool) = boolean(Bool).
+
+init_array(List) = array(List).
+
+init_object(AssocList) = object(AssocList).
+
+init_null = null.
+
+is_integer(integer(_)).
+
+is_float(float(_)).
+
+is_string(string(_)).
+
+is_boolean(boolean(_)).
+
+is_list(array(_)).
+
+is_object(object(_)).
+
+is_null(null).
+
+integer(integer(Int)) = Int.
+
+% Implement det_integer/1!!!
+
+float(float(Float)) = Float.
+
+% Implement det_float/1!!!
+
+string(string(String)) = String.
+
+% Implement det_string/1!!!
+
+boolean(boolean(Bool)) = Bool.
+
+% Implement det_boolean/1!!!
+
+array(array(List)) = List.
+
+% Implement det_list/1!!!
+
+object(object(Object)) = Object.
+
+% Implement det_object/1!!!
+
+to_string(JSON, Format) = to_string(JSON, Format, 1).
+
+:- func to_string(json, format, int) = string.
+
+to_string(integer(Int), _, _) = int_to_string(Int).
+to_string(float(Float), _, _) = float_to_string(Float).
+to_string(string(String), _, _) = """" ++ String ++ """".
+to_string(boolean(no), _, _) = "false".
+to_string(boolean(yes), _, _) = "true".
+to_string(array(List), Format @ format(_, compact), Layer) =
+	"[" ++ foldl((func(JSON, Before) =
+		(	if Before = ""
+			then to_string(JSON, Format, Layer)
+			else Before ++ ", " ++ to_string(JSON, Format, Layer)
+		)
+	), List, "") ++ "]".
+to_string(array(List), Format @ format(_, multiline(Indent, Newline)), Layer) =
+	"[" ++ newline_string(Newline) ++ foldl((func(JSON, Before) =
+		(	if Before = ""
+			then from_char_list(duplicate(Indent * Layer, ' ')) ++ to_string(JSON, Format, Layer + 1)
+			else Before ++ "," ++ newline_string(Newline) ++ from_char_list(duplicate(Indent * Layer, ' ')) ++ to_string(JSON, Format, Layer + 1)
+		)
+	), List, "") ++ newline_string(Newline) ++ from_char_list(duplicate(Indent * (Layer - 1), ' ')) ++ "]".
+to_string(object(AssocList), Format @ format(compact, _), Layer) =
+	"{" ++ foldl((func((Key - JSON), Before) =
+		(	if Before = ""
+			then """" ++ Key ++ """: " ++ to_string(JSON, Format, Layer)
+			else Before ++ ", """ ++ Key ++ """: " ++ to_string(JSON, Format, Layer)
+		)
+	), AssocList, "") ++ "}".
+to_string(object(AssocList), Format @ format(multiline(Indent, Newline), _), Layer) =
+	"{" ++ newline_string(Newline) ++ foldl((func((Key - JSON), Before) =
+		(	if Before = ""
+			then from_char_list(duplicate(Indent * Layer, ' ')) ++ """" ++ Key ++ """: " ++ to_string(JSON, Format, Layer + 1)
+			else Before ++ "," ++ newline_string(Newline) ++ from_char_list(duplicate(Indent * Layer, ' ')) ++ """" ++ Key ++ """: " ++ to_string(JSON, Format, Layer + 1)
+		)
+	), AssocList, "") ++ newline_string(Newline) ++ from_char_list(duplicate(Indent * (Layer - 1), ' ')) ++ "}".
+to_string(null, _, _) = "null".
+
+:- func newline_string(newline) = string.
+
+newline_string(crlf) = "\r\n".
+newline_string(cr) = "\r".
+newline_string(lf) = "\n".
+
+write_json(JSON, Format) --> write_string(to_string(JSON, Format)).
+
+write_json(Stream, JSON, Format) --> write_string(Stream, to_string(JSON, Format)).
+
+json_parser(Src, JSON, !PS) :-
+	(	int_literal(Src, Int, !PS)
+	->	JSON = integer(Int)
+	;	float_literal(Src, Float, !PS)
+	->	JSON = float(Float)
+	;	string_literal('"', Src, String, !PS)
+	->	JSON = string(String)
+	;	keyword("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz", "true", Src, _, !PS)
+	->	JSON = boolean(yes)
+	;	keyword("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz", "false", Src, _, !PS)
+	->	JSON = boolean(no)
+	;	brackets("[", "]", comma_separated_list(json_parser), Src, List, !PS)
+	->	JSON = array(List)
+	;	brackets("{", "}", comma_separated_list(object_entry_parser), Src, AssocList, !PS)
+	->	JSON = object(AssocList)
+	;	keyword("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz", "null", Src, _, !PS)
+	->	JSON = null
+	;	fail
+	).
+
+:- pred object_entry_parser(src::in, pair(string, json)::out, ps::in, ps::out) is semidet.
+
+object_entry_parser(Src, String - JSON) --> string_literal('"', Src, String), punct(":", Src, _), json_parser(Src, JSON).
+
+parse_json_string(String, ParseResult) :- parse(String, json_parser, ParseResult).
